51891e1ed39bd3cb9dcdd6cbe69a3911fcc542ae
Munetsi01 <lazarus.munetsi@liquid.tech>
2025-08-02 15:04:04 +0200

    added Dashboard

--- frontend/tms-app/src/components/Dashboard.js
+++ frontend/tms-app/src/components/Dashboard.js
@@ -1,26 +1,8 @@
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import AuthContext from "../context/AuthProvider";
-
 const Dashboard = () => {
-    const { setAuth } = useContext(AuthContext);
-    const navigate = useNavigate();
-
-    const logout = async () => {
-        // if used in more components, this should be in context 
-        // axios to /logout endpoint 
-        setAuth({});
-        navigate('/');
-    }
 
     return (
         <section>
-            <h1>Home</h1>
-            <br />
-            <p>Welcome To Dashboard</p>
-            <div className="flexGrow">
-                <button onClick={logout}>Sign Out</button>
-            </div>
+           <h1>Dashboard</h1>
         </section>
     )
 }
