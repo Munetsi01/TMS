2e7242995052424ae3350c209d014653df2f5832
Munetsi01 <lazarus.munetsi@liquid.tech>
2025-08-02 13:38:01 +0200

    WIP Home works

--- frontend/tms-app/src/components/Home.js
+++ frontend/tms-app/src/components/Home.js
@@ -1,49 +1,32 @@
-//import {useRef, useState, useEffect, useContext} from 'react';
-import axios from '../api/axios';
-
-const TASKS_URL = '/tasks';
+import { useNavigate, Link } from "react-router-dom";
+import { useContext } from "react";
+import AuthContext from "../context/AuthProvider";
 
 const Home = () => {
+    const { setAuth } = useContext(AuthContext);
+    const navigate = useNavigate();
 
-    try{
-        //     const response = axios.get(TASKS_URL, 
-        //     {
-        //        headers : {'Content-Type':'application/json'}
-        //     });
-    
-        // console.log(JSON.stringify(response?.data));
-        //success
-        // const accessToken = response?.data?.Token;
-        // setAuth({user,pwd,accessToken});
-    
-        //  setUser('');
-        //  setPwd('');
-        //  setSuccess(true);
-         }
-         catch(err){
-           if(!err?.response){
-            //setErrMsg("No Server Response")
-           }
-           else if(err.response?.status === 400){
-                    //setErrMsg(err?.response?.data?.message);
-           }
-            else if(err.response?.status === 401){
-                   // setErrMsg(err?.response?.data?.message);
-           }
-            else if(err.response?.status === 403){
-                    //setErrMsg(err?.response?.data?.message);
-           }
-           else{
-                console.log(err?.response?.data);
-            //setErrMsg("Tasks Loading Falied")
-           }
-        }
-    return(
-     <div>
-        Home is under construction
-        Loading Tasks from background
-     </div>
-    )
-     
+    const logout = async () => {
+        // if used in more components, this should be in context 
+        // axios to /logout endpoint 
+        setAuth({});
+        navigate('/');
     }
+
+    return (
+        <section>
+            <h1>Home</h1>
+            <br />
+            <p>You are logged in!</p>
+            <br />
+            <Link to="/dashboard">Go to the Editor page</Link>
+            <br />
+            <Link to="/users">Go to the Admin page</Link>
+            <div className="flexGrow">
+                <button onClick={logout}>Sign Out</button>
+            </div>
+        </section>
+    )
+}
+
 export default Home
